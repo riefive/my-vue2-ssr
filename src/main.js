67870268c49db51600e7e261cd1af20bf517805e
riefive <rie.five@gmail.com>
2020-11-10 05:11:36 +0700

    add entry client and server

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+export function createApp () {
+    const app = new Vue({
+        router,
+        store,
+        render: h => h(App)
+    })
+    return { app, router, store }
+}
