13e96abf42ffbde1c035f8efe673705262bc7af4
riefive <rie.five@gmail.com>
2020-11-11 10:31:35 +0700

    add layouts

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Default Layout</h1>
+        <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'default-layout'
+}
+</script>
---
+++ src/layouts/LayoutMobile.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Mobile Layout</h1>
+        <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'mobile-layout'
+}
+</script>
