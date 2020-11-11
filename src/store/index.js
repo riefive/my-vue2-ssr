8699941ea52bd1abb02c4abed1b07a72d0dff6b6
riefive <rie.five@gmail.com>
2020-11-11 09:59:36 +0700

    add module breakpoint at store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import ModuleBreakpoint from './module-breakpoint'
 
 Vue.use(Vuex)
 
@@ -11,5 +12,6 @@ export default new Vuex.Store({
     actions: {
     },
     modules: {
+        breakpoint: ModuleBreakpoint
     }
 })
---
+++ src/store/module-breakpoint.js
@@ -0,0 +1,31 @@
+export default {
+    state: {
+        name: 'xs',
+        orientation: 'portrait',
+        width: 0,
+        height: 0
+    },
+    mutations: {
+        changeName(state, value) {
+            state.name = value
+        },
+        changeOrientation(state, value) {
+            state.orientation = value
+        },
+        changePixels(state, value) {
+            state.width = 'width' in value ? value.width : 0
+            state.height = 'height' in value ? value.height : 0
+        }
+    },
+    actions: {
+        updateName({ commit }, value) {
+            commit('changeName', value)
+        },
+        updateOrientation({ commit }, value) {
+            commit('changeOrientation', value)
+        },
+        updatePixels({ commit }, value) {
+            commit('changePixels', value)
+        }
+    }
+}
